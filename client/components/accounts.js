0d3267169a0ded7079b88374fa195f21e8e8ca73
Brown Fitterman <j.b.fitterman@gmail.com>
2017-05-13 18:04:03 +0530

    setting up all the files

---
+++ client/components/accounts.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Accounts extends Component {
+  componentDidMount() {
+    // Render the Blaze accounts form then find the div
+    // We just rendered in the "render" method and place
+    // The blaze accounts form in that div.
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Accounts;
